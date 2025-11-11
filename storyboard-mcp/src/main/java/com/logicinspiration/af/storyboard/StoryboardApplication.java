5ccb04213e12befd6a9d225b9d2a039c30c151a8
Chris Ru <chris.ru@gmail.com>
2025-11-11 16:30:05 -0600

    Add storyboard-mcp project skeleton

---
+++ storyboard-mcp/src/main/java/com/logicinspiration/af/storyboard/StoryboardApplication.java
@@ -0,0 +1,12 @@
+package com.logicinspiration.af.storyboard;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@SpringBootApplication
+public class StoryboardApplication {
+    public static void main(String[] args) {
+        run(StoryboardApplication.class, args);
+    }
+}
